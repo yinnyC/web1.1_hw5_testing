5fcc717f9a2ae02cd10319904536fdee82fc811f
Meredith Murphy <meredithcat@gmail.com>
2019-11-11 22:42:59 -0800

    update starter code

--- string_tests.py
+++ string_tests.py
@@ -3,9 +3,22 @@ import unittest
 from string_functions import *
 
 class StringFunctionTests(unittest.TestCase):
-    def test_default_greeting(self):
+    def test_greeting(self):
         """Tests for greet_by_name"""
-        self.assertEqual(greet_by_name('Dani'), 'Hello, Meredith!')
+        self.assertEqual(greet_by_name('Jeremy'), 'Hello, Jeremy!')
+        self.assertEqual(greet_by_name('Dani'), 'Hello Dani!')
+
+    def test_reverse(self):
+        """Test reversing a string."""
+        pass
+
+    def test_reverse_words(self):
+        """Test reversing words in a string."""
+        pass
+
+    def test_sarcastic(self):
+        """Test sarcastic-ifying a string."""
+        pass
 
 # run the tests
 if __name__ == '__main__':
